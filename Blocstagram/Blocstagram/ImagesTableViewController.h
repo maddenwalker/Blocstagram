81687e28c38310a7181236aef82fdcaeb4ef6524
Ryan Walker <ryan@maddenwalker.com>
2015-10-07 12:55:56 -0700

    Checkpoint 27 Building Blocstagram, Completed

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Ryan Walker on 10/7/15.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ImagesTableViewController : UITableViewController
 
+@property (strong, nonatomic) NSMutableArray *images;
 
 @end
-
