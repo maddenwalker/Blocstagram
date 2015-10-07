f1be5336cc1669829d68f6cad07dbba6d35810bb
Ryan Walker <ryan@maddenwalker.com>
2015-10-07 12:29:26 -0700

    Initial Blocstagram Commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Ryan Walker on 10/7/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Ryan Walker on 10/7/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
