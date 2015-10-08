a515e3a251229a8d55b3bb36084009596e196b60
Ryan Walker <ryan@maddenwalker.com>
2015-10-08 14:00:03 -0700

    Checkpoint 32 Infinite Scroll and Pull to Refresh, Completed

--- Blocstagram/Blocstagram/DataSource.h
+++ Blocstagram/Blocstagram/DataSource.h
@@ -10,11 +10,15 @@
 
 @class Media;
 
+typedef void (^NewItemCompletionBlock)(NSError *error);
+
 @interface DataSource : NSObject
 
 +(instancetype) sharedInstance;
 @property (strong, nonatomic, readonly) NSArray *mediaItems;
 - (void) deleteMediaItem:(Media *)item;
 - (void) moveMediaItemToFirstInArray:(Media *)item;
+- (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
+- (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 
 @end
--- Blocstagram/Blocstagram/DataSource.m
+++ Blocstagram/Blocstagram/DataSource.m
@@ -15,6 +15,10 @@
     NSMutableArray *_mediaItems;
 }
 
+@property (strong, nonatomic) NSArray *mediaItems;
+@property (assign, nonatomic) BOOL isRefreshing;
+@property (assign, nonatomic) BOOL isLoadingOlderItems;
+
 @end
 
 @implementation DataSource
@@ -77,6 +81,48 @@
     [mutableArrayWithKVO insertObject:item atIndex:0];
 }
 
+#pragma mark - completion handler methods
+
+- (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler {
+    if (self.isRefreshing == NO) {
+        self.isRefreshing = YES;
+        
+        Media *media = [[Media alloc] init];
+        media.user = [self randomUser];
+        media.image = [UIImage imageNamed:@"10.jpg"];
+        media.caption = [self randomSentence];
+        
+        NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+        [mutableArrayWithKVO insertObject:media atIndex:0];
+        
+        self.isRefreshing = NO;
+        
+        if (completionHandler) {
+            completionHandler(nil);
+        }
+    }
+}
+
+- (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler {
+    if (self.isLoadingOlderItems == NO) {
+        self.isLoadingOlderItems = YES;
+        
+        Media *media = [[Media alloc] init];
+        media.user = [self randomUser];
+        media.image = [UIImage imageNamed:@"1.jpg"];
+        media.caption = [self randomSentence];
+        
+        NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+        [mutableArrayWithKVO addObject:media];
+        
+        self.isLoadingOlderItems = NO;
+        
+        if (completionHandler) {
+            completionHandler(nil);
+        }
+    }
+}
+
 #pragma mark - random data creation
 - (void) addRandomData {
     NSMutableArray *randomMediaItems = [NSMutableArray array];
@@ -105,7 +151,7 @@
         }
     }
     
-    _mediaItems = randomMediaItems;
+    self.mediaItems = randomMediaItems;
 }
 
 - (User *) randomUser {
