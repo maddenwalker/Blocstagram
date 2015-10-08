0486288c27e933b6a99ccff4b10498ccb739d255
Ryan Walker <ryan@maddenwalker.com>
2015-10-08 16:32:00 -0700

    Checkpoint 33 Connecting to Instagram, Completed

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Ryan Walker on 10/8/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
